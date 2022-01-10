b521d8430a720e8b30d4aaca2499474e3e975250
Bhargavi Nalluri <b.nalluri@f5.com>
2022-01-10 14:19:29 +0530

    fix: changed hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func Hello() {
 	fmt.Println("Hello All!!")
-	fmt.Println("this is from hello package from golang module")
+	fmt.Println("this is from hello package from golang module-1.0.4")
 }
